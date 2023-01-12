d87961906b293a19574bdc850bb0bda2d8017951
Saleh BUD <saleh.7734@gmail.com>
2023-01-12 12:04:07 +0200

    Complete

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,25 @@
-// export const Article = () => ();
+import React from 'react';
+
+const dateISO = date => date.toISOString().slice(0, 10);
+
+const dateLocaleString = date => date.toLocaleDateString('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+});
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={dateISO(date)} data-cy="date">
+        {dateLocaleString(date)}
+      </time>
+    </p>
+
+    <p data-cy="text">{text}</p>
+  </div>
+);
